2738b3502560eb3fbf43071d5808ecd3156e8c93
Ioannis Karasavvaidis <john@studio110.eu>
2024-01-01 00:54:43 +0000

    Add auto-save

---
+++ lua/plugins/auto-save.lua
@@ -0,0 +1,20 @@
+return {
+  "pocco81/auto-save.nvim",
+  lazy = false,
+  config = function()
+    local autoSave = require("auto-save")
+    autoSave.setup({
+      enabled = true,
+      execution_message = {
+        message = function()
+          return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
+        end,
+        dim = 0.18,
+        cleaning_interval = 1250,
+      },
+      events = { "InsertLeave", "TextChanged" },
+      write_all_buffers = false,
+      debounce_delay = 135,
+    })
+  end,
+}
