e0d21edfc045ad55d654ad75f9f32021460907b6
Ioannis Karasavvaidis <jkarasavvaidis@gmail.com>
2024-01-02 13:44:43 +0000

    Add vim fugitive

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -56,6 +56,9 @@ return {
       lspconfig.gopls.setup({
         capabilites = capabilities,
       })
+      lspconfig.rust_analyzer.setup({
+        capabilites = capabilities,
+      })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
