7cf7d679a1bdd0a819e6593cfc3e65131b73c787
Ioannis Karasavvaidis <ioannis@puppet.com>
2024-08-09 11:06:59 +0100

    feat(keymaps): add navigation keymaps for split windows
    - Changed keymap for making splits equal size from <leader>se to <leader>s=
    - Added keymaps for navigating between split windows:
      - <leader>sn: Navigate to left split
      - <leader>se: Navigate to upper split
      - <leader>si: Navigate to lower split
      - <leader>so: Navigate to right split

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -11,8 +11,12 @@ keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decremen
 -- window management
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+keymap.set("n", "<leader>s=", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+keymap.set("n", "<leader>sn", "<C-w>h", { desc = "Navigate to left split" })
+keymap.set("n", "<leader>se", "<C-w>j", { desc = "Navigate to Upper split" })
+keymap.set("n", "<leader>si", "<C-w>k", { desc = "Navigate to Lower split" })
+keymap.set("n", "<leader>so", "<C-w>l", { desc = "Navigate to Right split" })
 
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
