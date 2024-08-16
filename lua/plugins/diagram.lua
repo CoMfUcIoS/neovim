de05a73894e9dfef25c972a01886426df36ba68c
Ioannis Karasavvaidis <ioannis@puppet.com>
2024-08-16 10:51:00 +0100

    feat(plugins): add diagram and image plugins
    - Added `diagram.nvim` plugin with optional dependencies and default options.
    - Added `image.nvim` plugin with configurations for markdown and neorg integrations.

---
+++ lua/plugins/diagram.lua
@@ -0,0 +1,14 @@
+return {
+	"3rd/diagram.nvim",
+	dependencies = {
+		"3rd/image.nvim",
+	},
+	opts = { -- you can just pass {}, defaults below
+		-- renderer_options = {
+		-- 	mermaid = {
+		-- 		background = nil, -- nil | "transparent" | "white" | "#hex"
+		-- 		theme = nil, -- nil | "default" | "dark" | "forest" | "neutral"
+		-- 	},
+		-- },
+	},
+}
