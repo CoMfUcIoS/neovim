c0d3c5fa8aeec6a5c76eef45252b58dd9a32a05f
Ioannis Karasavvaidis <ioannis@puppet.com>
2024-08-19 13:03:52 +0100

    feat(keymaps): add new key mappings for surrounding words with various characters
    - Changed quit key mapping from <leader>qa to <leader>qx
    - Added key mappings to surround words with double quotes, single quotes, parentheses, brackets, and braces
    - Added key mappings to change surrounding characters to single or double quotes
    
    BREAKING CHANGE: The key mapping for quitting has been changed from <leader>qa to <leader>qx

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -24,5 +24,13 @@ keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 
-keymap.set("n", "<leader>qa", "<cmd>qa<CR>", { desc = "Quit" }) -- quit
+keymap.set("n", "<leader>qx", "<cmd>qa<CR>", { desc = "Quit" }) -- quit
 keymap.set("n", "<leader>qb", "<cmd>bd<CR>", { desc = "Close buffer" })
+
+keymap.set("n", '<leader>qa"', 'ciw""<ESC>P', { desc = "Surround word with double quotes" })
+keymap.set("n", "<leader>qa'", "ciw''<ESC>P", { desc = "Surround word with single quotes" })
+keymap.set("n", "<leader>qa(", "ciw()<ESC>P", { desc = "Surround word with parentheses" })
+keymap.set("n", "<leader>qa[", "ciw[]<ESC>P", { desc = "Surround word with brackets" })
+keymap.set("n", "<leader>qa{", "ciw{}<ESC>P", { desc = "Surround word with braces" })
+keymap.set("n", "<leader>qc'", "ciw<DEl><BS>''<ESC>P", { desc = "Change surround to '" })
+keymap.set("n", '<leader>qc"', 'ciw<DEl><BS>""<ESC>P', { desc = 'Change surround to "' })
