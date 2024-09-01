9389962aa66e05c5b59729cedeebd63e266d9fb7
Ioannis Karasavvaidis <jkarasavvaidis@gmail.com>
2024-09-01 13:18:09 +0100

    feat(rest): add key mappings for Rest plugin commands
    - Added key mappings for running Rest requests, opening result pane, running the last request, and editing logs and cookies files.
    - Key mappings:
      - `<leader>rr`: Run Rest request under the cursor
      - `<leader>ro`: Open Rest result pane
      - `<leader>rp`: Run last Rest request
      - `<leader>rl`: Edit Rest logs file
      - `<leader>rc`: Edit Rest cookies file

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -8,4 +8,11 @@ return {
 			end,
 		})
 	end,
+	keys = {
+		{ "<leader>rr", ":Rest run<CR>", desc = "Run Rest request under the cursor" },
+		{ "<leader>ro", ":Rest open<CR>", desc = "Open Rest result pane" },
+		{ "<leader>rp", ":Rest last<CR>", desc = "Run last Rest request" },
+		{ "<leader>rl", ":Rest logs<CR>", desc = "Edit Rest logs file" },
+		{ "<leader>rc", ":Rest cookies<CR>", desc = "Edit Rest cookies file" },
+	},
 }
