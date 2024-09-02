4ec0cc52ef605b95ded689885d7f109848153801
Ioannis Karasavvaidis <ioannis@puppet.com>
2024-09-02 10:37:19 +0100

    fix(keymaps): change quit keymap from <leader>qx to <leader>qq
    Updated the keymap for quitting Neovim from <leader>qx to <leader>qq for consistency and ease of use.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -33,7 +33,7 @@ keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 
-keymap.set("n", "<leader>qx", "<cmd>qa<CR>", { desc = "Quit" }) -- quit
+keymap.set("n", "<leader>qq", "<cmd>qa<CR>", { desc = "Quit" }) -- quit
 keymap.set("n", "<leader>qb", "<cmd>bd<CR>", { desc = "Close buffer" })
 keymap.set("n", "<leader>qw", "<cmd>%bd<CR>", { desc = "Close all buffers" })
 
