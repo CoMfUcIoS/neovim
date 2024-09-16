482c86ebd5d2cf4f79103d5bb1e9f006836a3d29
Ioannis Karasavvaidis <ioannis@puppet.com>
2024-09-16 11:34:38 +0100

    chore: update lazy-lock.json and pomo.lua
    - Updated lazy-lock.json with new plugin commits and added new plugins.
    - Changed pomo.nvim plugin source from "epwalsh/pomo.nvim" to "comfucios/pomo.nvim".
    - Added "rcarriga/nvim-notify" as a dependency for pomo.nvim.
    - Removed unused timer and notifier configurations in pomo.lua.

--- lua/plugins/pomo.lua
+++ lua/plugins/pomo.lua
@@ -1,21 +1,17 @@
 return {
-	"epwalsh/pomo.nvim",
+	"comfucios/pomo.nvim",
 	version = "*", -- Recommended, use latest release instead of latest commit
 	lazy = true,
 	cmd = { "TimerStart", "TimerRepeat", "TimerSession" },
+	dependencies = {
+		"rcarriga/nvim-notify",
+	},
 	config = function()
 		local pomo = require("pomo")
 		local telescope = require("telescope")
 
 		pomo.setup({
 			update_interval = 1000,
-			timers = {},
-			notifiers = {
-				name = "Default",
-				opts = {
-					sticky = false,
-				},
-			},
 			sessions = {
 				pomodoro = {
 					{ name = "Work", duration = "25m" },
