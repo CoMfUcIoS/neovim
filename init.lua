f1fe79de1acfd7146f66c9104be71e39f3b42759
Ioannis Karasavvaidis <jkarasavvaidis@gmail.com>
2024-09-20 16:31:43 +0100

    feat: update avante plugin build command
    - Change build command to `make BUILD_FROM_SOURCE=true` for avante plugin.
    - Update lazy-lock.json with the latest commits for various plugins.
    - Add git configuration to lazy setup.

--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
-
 local function toggle_verbose()
 	if vim.o.verbose == 0 then
 		vim.o.verbosefile = vim.fn.expand("~/.vim_verbose.log")
@@ -15,11 +14,17 @@ if vim.g.vscode then
 	-- echo something dont leave this empty
 	print("VSCode mode enabled")
 else
-	require("lazy").setup({ 
-		{ 
-			import = "plugins" 
-		} 
+	require("lazy").setup({
+		{
+			import = "plugins",
+		},
 	}, {
+		git = {
+			log = { "-8" }, -- show the last 8 commits
+			timeout = 300, -- kill processes that take more than 5 minutes
+			url_format = "https://github.com/%s.git",
+			filter = true,
+		},
 		checker = {
 			enabled = true,
 			notify = false,
