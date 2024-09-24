2edc9892438ab5999fb513ae3a073d2a15e0fbf0
Ioannis Karasavvaidis <ioannis@puppet.com>
2024-09-24 09:59:24 +0100

    feat(plugins): update copilot dependencies and add new copilot plugin configuration
    - Removed `zbirenbaum/copilot.lua` from `avante.lua` dependencies.
    - Updated `plenary.nvim` dependency in `copilot-chat.lua`.
    - Added new `copilot.lua` plugin configuration with specific options for markdown and help filetypes.

--- lua/plugins/copilot-chat.lua
+++ lua/plugins/copilot-chat.lua
@@ -21,8 +21,7 @@ return {
 	"CopilotC-Nvim/CopilotChat.nvim",
 	branch = "canary",
 	dependencies = {
-		{ "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
-		{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+		{ "Nvim-lua/plenary.nvim" }, -- for curl, log wrapper
 		{ "nvim-telescope/telescope.nvim" }, -- for telescope integration
 	},
 	opts = {
