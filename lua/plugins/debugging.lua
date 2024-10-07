c822c4d4b5dcc1c32f8c08adc875640c0817d347
Ioannis Karasavvaidis <ioannis@puppet.com>
2024-10-07 13:32:26 +0100

    feat(debugging): enhance nvim-dap setup with additional configurations and dependencies
    - Added `nvim-dap-virtual-text` and `nvim-dap-vscode-js` plugins.
    - Included `vscode-js-debug` with necessary build steps.
    - Configured `dap-vscode-js` for various adapters including `pwa-node`, `pwa-chrome`, `pwa-msedge`, `node-terminal`, and `pwa-extensionHost`.
    - Added debug configurations for `typescript`, `javascript`, `typescriptreact`, and `javascriptreact`.
    - Integrated `json5` parser for `dap.ext.vscode`.
    - Enabled DAP support in `overseer`.
    
    This enhances the debugging capabilities within Neovim, providing better support for JavaScript and TypeScript debugging.

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -3,14 +3,20 @@ return {
 		"mfussenegger/nvim-dap",
 		dependencies = {
 			"leoluz/nvim-dap-go",
-			"rcarriga/nvim-dap-ui",
-			"nvim-neotest/nvim-nio",
+			{ "rcarriga/nvim-dap-ui", dependencies = { "nvim-neotest/nvim-nio" }, config = true },
 			{
 				"b1nhack/nvim-json5",
 				-- if you're on windows
 				-- run = 'powershell ./install.ps1'
 				build = "./install.sh",
 				lazy = false,
+			},
+			{ "theHamsta/nvim-dap-virtual-text", config = true },
+			{ "mxsdev/nvim-dap-vscode-js", module = { "dap-vscode-js" } },
+			{
+				"microsoft/vscode-js-debug",
+				opt = true,
+				run = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
 			},
 		},
 		config = function()
@@ -45,6 +51,79 @@ return {
 				},
 			}
 
+			local DEBUGGER_PATH = os.getenv("HOME") .. "/.local/share/nvim/lazy/vscode-js-debug"
+
+			require("dap-vscode-js").setup({
+				node_path = "node",
+				debugger_path = DEBUGGER_PATH,
+				debugger_cmd = { "js-debug-adapter" },
+				log_file_path = os.getenv("HOME") .. "/.local/share/nvim/dap_vscode_js.log",
+				log_file_level = 2,
+				log_console_level = 2,
+				adapters = {
+					"pwa-node",
+					"pwa-chrome",
+					"pwa-msedge",
+					"node-terminal",
+					"pwa-extensionHost",
+				}, -- which adapters to register in nvim-dap
+			})
+
+			for _, language in ipairs({ "typescript", "javascript" }) do
+				dap.configurations[language] = {
+					{
+						type = "pwa-node",
+						request = "launch",
+						name = "Launch file",
+						program = "${file}",
+						cwd = "${workspaceFolder}",
+					},
+					{
+						type = "pwa-node",
+						request = "attach",
+						name = "Attach",
+						processId = require("dap.utils").pick_process,
+						cwd = "${workspaceFolder}",
+					},
+					{
+						type = "pwa-node",
+						request = "launch",
+						name = "Debug Jest Tests",
+						-- trace = true, -- include debugger info
+						runtimeExecutable = "node",
+						runtimeArgs = {
+							"./node_modules/jest/bin/jest.js",
+							"--runInBand",
+						},
+						rootPath = "${workspaceFolder}",
+						cwd = "${workspaceFolder}",
+						console = "integratedTerminal",
+						internalConsoleOptions = "neverOpen",
+					},
+				}
+			end
+
+			for _, language in ipairs({ "typescriptreact", "javascriptreact" }) do
+				dap.configurations[language] = {
+					{
+						type = "pwa-chrome",
+						name = "Attach - Remote Debugging",
+						request = "attach",
+						program = "${file}",
+						cwd = vim.fn.getcwd(),
+						sourceMaps = true,
+						protocol = "inspector",
+						port = 9222,
+						webRoot = "${workspaceFolder}",
+					},
+					{
+						type = "pwa-chrome",
+						name = "Launch Chrome",
+						request = "launch",
+						url = "http://localhost:3000",
+					},
+				}
+			end
 			require("dap.ext.vscode").json_decode = require("json5").parse
 			require("overseer").enable_dap()
 			dap.listeners.before.attach.dapui_config = function()
