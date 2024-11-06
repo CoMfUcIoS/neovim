c5e1c95f4116dabac6b661147e77eab95c2a7ea9
Ioannis Karasavvaidis <ioannis@puppet.com>
2024-11-06 13:51:20 +0000

    feat(aider-chat): add configuration for aider.nvim plugin
    - Added `aider.nvim` plugin configuration with `auto_manage_context` and `default_bindings` set to true.
    - Set the model to `xai/grok-beta`.
    - Added key bindings for opening Aider and running Aider in the background.

---
+++ lua/plugins/aider-chat.lua
@@ -0,0 +1,26 @@
+return {
+	"joshuavial/aider.nvim",
+	config = function()
+		require("aider").setup({
+			auto_manage_context = true,
+			default_bindings = true,
+			model = "xai/grok-beta",
+		})
+	end,
+	keys = {
+		{
+			"<leader>aw",
+			function()
+				require("aider").AiderOpen(" --model xai/grok-beta")
+			end,
+			desc = "Aider Open",
+		},
+		{
+			"<leader>ab",
+			function()
+				require("aider").AiderBackground()
+			end,
+			desc = "Aider Background",
+		},
+	},
+}
