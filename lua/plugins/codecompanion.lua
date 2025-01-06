b411618bdaa156edae0a42655477768527a2e94e
Ioannis Karasavvaidis <ioannis@puppet.com>
2025-01-06 18:02:42 +0000

    feat: add codecompanion.nvim plugin
    - Added codecompanion.nvim plugin to lazy-lock.json
    - Created configuration file for codecompanion.nvim in lua/plugins/codecompanion.lua
    - Set up key mappings for CodeCompanion actions and chat

---
+++ lua/plugins/codecompanion.lua
@@ -0,0 +1,25 @@
+return {
+	"olimorris/codecompanion.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-treesitter/nvim-treesitter",
+	},
+	config = function()
+		require("codecompanion").setup({})
+
+		local keymaps = {
+			{ "n", "<C-a>", "<cmd>CodeCompanionActions<cr>" },
+			{ "v", "<C-a>", "<cmd>CodeCompanionActions<cr>" },
+			{ "n", "<LocalLeader>a", "<cmd>CodeCompanionChat Toggle<cr>" },
+			{ "v", "<leader>zz", "<cmd>CodeCompanionChat Toggle<cr>" },
+			{ "v", "ga", "<cmd>CodeCompanionChat Add<cr>" },
+		}
+
+		for _, keymap in ipairs(keymaps) do
+			vim.api.nvim_set_keymap(keymap[1], keymap[2], keymap[3], { noremap = true, silent = true })
+		end
+
+		-- Expand 'cc' into 'CodeCompanion' in the command line
+		vim.cmd([[cab cc CodeCompanion]])
+	end,
+}
