cc85023827ace1b11fc7d244b97aadf9a80ee6c3
Ioannis Karasavvaidis <ioannis@puppet.com>
2025-07-08 11:59:07 +0300

    feat(folds): switch to origami, remove ufo & fold_line
    Replaced nvim-ufo and fold_line.nvim with nvim-origami for folding.
    Updated keymaps and configuration to use origami features, and removed
    dependencies and settings related to the old plugins. This simplifies
    fold management and improves compatibility.

--- lua/plugins/fold.lua
+++ lua/plugins/fold.lua
@@ -1,68 +1,35 @@
 return {
 	{
-		"kevinhwang91/nvim-ufo",
-		event = "VeryLazy",
-		dependencies = { "nvim-treesitter/nvim-treesitter", "kevinhwang91/promise-async" },
-		keys = {
-			{ "zR", '<Cmd>lua require("ufo").openAllFolds()<CR>', "ufo: open all folds" },
-			{ "zM", '<Cmd>lua require("ufo").closeAllFolds()<CR>', "ufo: close all folds" },
-			{ "zK", '<Cmd>lua require("ufo").peekFoldedLinesUnderCursor()<CR>', "ufo: preview fold" },
-		},
-		config = function()
-			vim.o.foldcolumn = "1"
-			vim.o.foldlevel = 99
-			vim.o.foldlevelstart = 99
-			vim.o.foldenable = true
-
-			require("ufo").setup({
-				open_fold_hl_timeout = 150,
-				preview = {
-					win_config = {
-						winhighlight = "NormalFloat:FloatBorder,FloatBorder:FloatBorder",
-					},
-				},
-				enable_get_fold_virt_text = true,
-				provider_selector = function(bufnr, filetype, buftype)
-					return { "treesitter", "indent" }
-				end,
-				-- If you want to exclude filetypes, add this here:
-				-- filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
-			})
-		end,
-	},
-	{
 		"chrisgrieser/nvim-origami",
-		version = "v1.9",
 		event = "VeryLazy",
-		keys = {
-			{
-				"<Left>",
-				function()
-					require("origami").h()
-				end,
-				desc = "Close fold",
+		opts = {
+			useLspFoldsWithTreesitterFallback = true,
+			pauseFoldsOnSearch = true,
+			foldtext = {
+				enabled = true,
+				padding = 3,
+				lineCount = {
+					template = "%d lines",
+					hlgroup = "Comment",
+				},
+				diagnosticsCount = true,
+				gitsignsCount = true,
 			},
-			{
-				"<Right>",
-				function()
-					require("origami").l()
-				end,
-				desc = "Open fold",
+			autoFold = {
+				enabled = true,
+				kinds = { "comment", "imports" },
 			},
+			foldKeymaps = {
+				setup = false, -- we'll set custom keymaps below
+				hOnlyOpensOnFirstColumn = false,
+			},
+			signs = { open = "", closed = "" },
 		},
-		config = function()
-			require("origami").setup({
-				setupFoldKeymaps = false,
-				signs = { open = "", closed = "" },
-			})
-		end,
-	},
-	{
-		"gh-liu/fold_line.nvim",
-		event = "VeryLazy",
 		init = function()
-			vim.g.fold_line_char_open_start = "╭"
-			vim.g.fold_line_char_open_end = "╰"
+			vim.opt.foldlevel = 99
+			vim.opt.foldlevelstart = 99
+			vim.keymap.set("n", "<Left>", function() require("origami").h() end, { desc = "Close fold" })
+			vim.keymap.set("n", "<Right>", function() require("origami").l() end, { desc = "Open fold" })
 		end,
 	},
 }
